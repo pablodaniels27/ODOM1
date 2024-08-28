e353b1f026402a8ef82ee027f75550a51ce2962e
Soul <soulofyokai08@gmail.com>
2024-08-28 16:30:47 -0500

    monitoreo ya todos los botones tienen funcionalidad

--- pruebafinal/src/controllers/MonitoreoController.java
+++ pruebafinal/src/controllers/MonitoreoController.java
@@ -67,6 +67,10 @@ public class MonitoreoController {
     @FXML
     private CheckBox empleadosCheckBox;
 
+    @FXML
+    private TextField searchField;
+
+
     private ObservableList<Map<String, Object>> employees = FXCollections.observableArrayList();
 
     private int itemsPerPage = 10;
@@ -89,6 +93,13 @@ public class MonitoreoController {
         employeeTableView.setItems(employees);
 
         cargarDepartamentos(); // Cargar los departamentos en el ChoiceBox
+
+        // Cargar entradas más recientes por defecto
+        try {
+            loadRecentEntries();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         // Configurar el botón de búsqueda
         searchButton.setOnAction(event -> {
@@ -126,92 +137,90 @@ public class MonitoreoController {
 
 
     private void searchByDateAndDepartment() throws SQLException {
-        LocalDate fechaInicio = fechaInicioPicker.getValue();
-        LocalDate fechaFin = fechaFinPicker.getValue();
         String departamentoSeleccionado = departamentoChoiceBox.getSelectionModel().getSelectedItem();
+        String searchQuery = searchField.getText().trim();  // Obtener el valor del campo de búsqueda
 
-        if (fechaInicio != null && fechaFin != null) {
-            employees.clear();
+        employees.clear();
 
-            String fechaInicioSQL = fechaInicio.toString();
-            String fechaFinSQL = fechaFin.toString();
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
 
-            DatabaseConnection connectNow = new DatabaseConnection();
-            Connection connectDB = connectNow.getConnection();
+        // Base de la consulta
+        String query = "SELECT e.nombres, e.apellido_paterno, e.apellido_materno, es.nombre as estado, " +
+                "dias.fecha, es.id as estado_id, en.hora_entrada, en.hora_salida, t.nombre as tipo_asistencia, ts.nombre as tipo_salida " +
+                "FROM entradas_salidas en " +
+                "JOIN empleados e ON en.empleado_id = e.id " +
+                "JOIN dias ON en.dia_id = dias.id " +
+                "JOIN estatus_empleado es ON e.estatus_id = es.id " +
+                "JOIN tipos_asistencia t ON en.tipo_asistencia_id = t.id " +
+                "JOIN tipos_salida ts ON en.tipo_salida_id = ts.id ";
 
-            // Base de la consulta
-            String query = "SELECT e.nombres, e.apellido_paterno, e.apellido_materno, es.nombre as estado, " +
-                    "dias.fecha, es.id as estado_id, en.hora_entrada, en.hora_salida, t.nombre as tipo_asistencia, ts.nombre as tipo_salida " +
-                    "FROM entradas_salidas en " +
-                    "JOIN empleados e ON en.empleado_id = e.id " +
-                    "JOIN dias ON en.dia_id = dias.id " +
-                    "JOIN estatus_empleado es ON e.estatus_id = es.id " +
-                    "JOIN tipos_asistencia t ON en.tipo_asistencia_id = t.id " +
-                    "JOIN tipos_salida ts ON en.tipo_salida_id = ts.id ";
+        // Filtros de departamento, supervisores, empleados y búsqueda por nombre/apellido
+        boolean filtroDepartamento = !departamentoSeleccionado.equals("Todos los departamentos");
+        boolean filtroSupervisores = supervisoresCheckBox.isSelected();
+        boolean filtroEmpleados = empleadosCheckBox.isSelected();
+        boolean filtroFechas = fechaInicioPicker.getValue() != null && fechaFinPicker.getValue() != null;
 
-            // Filtros de departamento, supervisores y empleados
-            boolean filtroDepartamento = !departamentoSeleccionado.equals("Todos los departamentos");
-            boolean filtroSupervisores = supervisoresCheckBox.isSelected();
-            boolean filtroEmpleados = empleadosCheckBox.isSelected();
+        if (filtroDepartamento || filtroSupervisores || filtroEmpleados || !searchQuery.isEmpty() || filtroFechas) {
+            query += "WHERE ";
 
-            if (filtroDepartamento || filtroSupervisores || filtroEmpleados) {
-                query += "WHERE ";
-
-                if (filtroDepartamento) {
-                    query += "e.departamento_id IN (SELECT id FROM departamentos WHERE nombre = '" + departamentoSeleccionado + "') ";
-                }
-
-                if (filtroSupervisores) {
-                    if (filtroDepartamento) query += "AND ";
-                    query += "e.jerarquia_id = 2 ";  // ID de Supervisores
-                }
-
-                if (filtroEmpleados) {
-                    if (filtroDepartamento || filtroSupervisores) query += "AND ";
-                    query += "e.jerarquia_id = 3 ";  // ID de Empleados
-                }
-
-                query += "AND dias.fecha BETWEEN '" + fechaInicioSQL + "' AND '" + fechaFinSQL + "'";
-            } else {
-                // Sin filtro de departamento, supervisores o empleados
-                query += "WHERE dias.fecha BETWEEN '" + fechaInicioSQL + "' AND '" + fechaFinSQL + "'";
+            if (filtroDepartamento) {
+                query += "e.departamento_id IN (SELECT id FROM departamentos WHERE nombre = '" + departamentoSeleccionado + "') ";
             }
 
-            System.out.println("Ejecutando consulta SQL: " + query);
-
-            try {
-                Statement statement = connectDB.createStatement();
-                ResultSet resultSet = statement.executeQuery(query);
-
-                while (resultSet.next()) {
-                    Map<String, Object> employeeData = new HashMap<>();
-                    employeeData.put("nombreCompleto", resultSet.getString("nombres") + " " + resultSet.getString("apellido_paterno") + " " + resultSet.getString("apellido_materno"));
-                    employeeData.put("id", resultSet.getInt("estado_id") != 0 ? String.valueOf(resultSet.getInt("estado_id")) : "");
-                    employeeData.put("fechaEntrada", resultSet.getString("fecha") != null ? resultSet.getString("fecha") : "");
-                    employeeData.put("horaEntrada", resultSet.getString("hora_entrada") != null ? resultSet.getString("hora_entrada") : "");
-                    employeeData.put("horaSalida", resultSet.getString("hora_salida") != null ? resultSet.getString("hora_salida") : "");
-                    employeeData.put("tipoAsistencia", resultSet.getString("tipo_asistencia") != null ? resultSet.getString("tipo_asistencia") : "");
-                    employeeData.put("tipoSalida", resultSet.getString("tipo_salida") != null ? resultSet.getString("tipo_salida") : "");
-                    employeeData.put("estado", resultSet.getString("estado") != null ? resultSet.getString("estado") : "");
-
-                    employees.add(employeeData);
-                }
-
-                connectDB.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (filtroSupervisores) {
+                if (filtroDepartamento) query += "AND ";
+                query += "e.jerarquia_id = 2 ";  // ID de Supervisores
             }
 
-            // Mostrar la primera página de resultados
-            showPage(1);
-        } else {
-            Alert alert = new Alert(Alert.AlertType.WARNING);
-            alert.setTitle("Advertencia");
-            alert.setHeaderText("Datos incompletos");
-            alert.setContentText("Por favor, selecciona un rango de fechas.");
-            alert.showAndWait();
+            if (filtroEmpleados) {
+                if (filtroDepartamento || filtroSupervisores) query += "AND ";
+                query += "e.jerarquia_id = 3 ";  // ID de Empleados
+            }
+
+            if (!searchQuery.isEmpty()) {
+                if (filtroDepartamento || filtroSupervisores || filtroEmpleados) query += "AND ";
+                query += "(e.nombres LIKE '%" + searchQuery + "%' OR e.apellido_paterno LIKE '%" + searchQuery + "%' OR e.apellido_materno LIKE '%" + searchQuery + "%') ";
+            }
+
+            if (filtroFechas) {
+                if (filtroDepartamento || filtroSupervisores || filtroEmpleados || !searchQuery.isEmpty()) query += "AND ";
+                String fechaInicioSQL = fechaInicioPicker.getValue().toString();
+                String fechaFinSQL = fechaFinPicker.getValue().toString();
+                query += "dias.fecha BETWEEN '" + fechaInicioSQL + "' AND '" + fechaFinSQL + "'";
+            }
         }
+
+        System.out.println("Ejecutando consulta SQL: " + query);
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Map<String, Object> employeeData = new HashMap<>();
+                employeeData.put("nombreCompleto", resultSet.getString("nombres") + " " + resultSet.getString("apellido_paterno") + " " + resultSet.getString("apellido_materno"));
+                employeeData.put("id", resultSet.getInt("estado_id") != 0 ? String.valueOf(resultSet.getInt("estado_id")) : "");
+                employeeData.put("fechaEntrada", resultSet.getString("fecha") != null ? resultSet.getString("fecha") : "");
+                employeeData.put("horaEntrada", resultSet.getString("hora_entrada") != null ? resultSet.getString("hora_entrada") : "");
+                employeeData.put("horaSalida", resultSet.getString("hora_salida") != null ? resultSet.getString("hora_salida") : "");
+                employeeData.put("tipoAsistencia", resultSet.getString("tipo_asistencia") != null ? resultSet.getString("tipo_asistencia") : "");
+                employeeData.put("tipoSalida", resultSet.getString("tipo_salida") != null ? resultSet.getString("tipo_salida") : "");
+                employeeData.put("estado", resultSet.getString("estado") != null ? resultSet.getString("estado") : "");
+
+                employees.add(employeeData);
+            }
+
+            connectDB.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Mostrar la primera página de resultados
+        showPage(1);
     }
+
+
 
 
     private Callback<TableColumn.CellDataFeatures<Map<String, Object>, String>, ObservableValue<String>> createCellValueFactory(String key) {
@@ -226,5 +235,52 @@ public class MonitoreoController {
         int fromIndex = (pageNumber - 1) * itemsPerPage;
         int toIndex = Math.min(fromIndex + itemsPerPage, employees.size());
         employeeTableView.setItems(FXCollections.observableArrayList(employees.subList(fromIndex, toIndex)));
+    }
+
+    private void loadRecentEntries() throws SQLException {
+        employees.clear();
+
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getConnection();
+
+        // Consulta SQL para obtener las entradas más recientes sin importar el día
+        String query = "SELECT e.nombres, e.apellido_paterno, e.apellido_materno, es.nombre as estado, " +
+                "dias.fecha, es.id as estado_id, en.hora_entrada, en.hora_salida, t.nombre as tipo_asistencia, ts.nombre as tipo_salida " +
+                "FROM entradas_salidas en " +
+                "JOIN empleados e ON en.empleado_id = e.id " +
+                "JOIN dias ON en.dia_id = dias.id " +
+                "JOIN estatus_empleado es ON e.estatus_id = es.id " +
+                "JOIN tipos_asistencia t ON en.tipo_asistencia_id = t.id " +
+                "JOIN tipos_salida ts ON en.tipo_salida_id = ts.id " +
+                "ORDER BY dias.fecha DESC, en.hora_entrada DESC " +
+                "LIMIT 10"; // Puedes ajustar el límite al número de entradas recientes que deseas mostrar
+
+        System.out.println("Ejecutando consulta SQL: " + query);
+
+        try {
+            Statement statement = connectDB.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Map<String, Object> employeeData = new HashMap<>();
+                employeeData.put("nombreCompleto", resultSet.getString("nombres") + " " + resultSet.getString("apellido_paterno") + " " + resultSet.getString("apellido_materno"));
+                employeeData.put("id", resultSet.getInt("estado_id") != 0 ? String.valueOf(resultSet.getInt("estado_id")) : "");
+                employeeData.put("fechaEntrada", resultSet.getString("fecha") != null ? resultSet.getString("fecha") : "");
+                employeeData.put("horaEntrada", resultSet.getString("hora_entrada") != null ? resultSet.getString("hora_entrada") : "");
+                employeeData.put("horaSalida", resultSet.getString("hora_salida") != null ? resultSet.getString("hora_salida") : "");
+                employeeData.put("tipoAsistencia", resultSet.getString("tipo_asistencia") != null ? resultSet.getString("tipo_asistencia") : "");
+                employeeData.put("tipoSalida", resultSet.getString("tipo_salida") != null ? resultSet.getString("tipo_salida") : "");
+                employeeData.put("estado", resultSet.getString("estado") != null ? resultSet.getString("estado") : "");
+
+                employees.add(employeeData);
+            }
+
+            connectDB.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Mostrar la primera página de resultados
+        showPage(1);
     }
 }
