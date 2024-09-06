f98d2a90aacf66c6a300f7fb02c1294b0432b621
Soul <soulofyokai08@gmail.com>
2024-09-06 15:37:00 -0500

    Modificacion de la clase usuario

--- pruebafinal/src/Usuarios/Empleado.java
+++ pruebafinal/src/Usuarios/Empleado.java
@@ -1,6 +1,14 @@
 package Usuarios;
 
-public class Empleado  {
+public class Empleado extends Usuario {
 
+    public Empleado(int id, String nombres, String apellidoMaterno, String apellidoPaterno, String fechaNacimiento,
+                    String pais, String ciudad, String correoElectronico, String lada, String telefono, String rfc,
+                    String curp, String profesion, int departamentoId, int jerarquiaId, int estatusId) {
+        super(id, nombres, apellidoMaterno, apellidoPaterno, fechaNacimiento, pais, ciudad, correoElectronico, lada, telefono, rfc, curp, profesion, departamentoId, jerarquiaId, estatusId);
+    }
+
+    // Métodos específicos de Empleado
 }
+
 
