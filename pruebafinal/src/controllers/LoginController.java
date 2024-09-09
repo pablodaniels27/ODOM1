cc3ecf7843720d3055bb8011299d63d3617dc229
Acksok <alexisnowmichael@gmail.com>
2024-09-09 15:57:19 -0600

    ya se arreglo TODO

--- pruebafinal/src/controllers/LoginController.java
+++ pruebafinal/src/controllers/LoginController.java
@@ -12,8 +12,14 @@ import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-
+import Services.CacheService;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
 
 public class LoginController {
 
@@ -31,7 +37,7 @@ public class LoginController {
     private Button loginButton;
 
     @FXML
-    private SplitPane splitPane; // Referencia al SplitPane en tu FXML
+    private SplitPane splitPane;
 
     private Stage primaryStage;
 
@@ -39,7 +45,6 @@ public class LoginController {
     public void initialize() {
         splitPane.setDividerPositions(0.3);
         splitPane.lookupAll(".split-pane-divider").forEach(div -> div.setMouseTransparent(true));
-        // Vincular el tamaño de la imagen al SplitPane para que se ajuste dinámicamente
         backgroundImage.fitWidthProperty().bind(splitPane.widthProperty());
         backgroundImage.fitHeightProperty().bind(splitPane.heightProperty());
     }
@@ -51,29 +56,8 @@ public class LoginController {
         }
     }
 
-    @FXML
-    private ImageView eyeIcon;
-
     public void setPrimaryStage(Stage stage) {
         this.primaryStage = stage;
-    }
-
-    @FXML
-    private void handleBackToPreLoginAction() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/PreLoginView.fxml"));
-            Parent root = loader.load();
-
-            PreLoginView preLoginController = loader.getController();
-            preLoginController.setPrimaryStage(primaryStage);
-
-            Scene scene = new Scene(root);
-            primaryStage.setScene(scene);
-            primaryStage.setTitle("ODOM. SA DE CV - PreLogin");
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     @FXML
@@ -82,6 +66,8 @@ public class LoginController {
         String password = passwordField.getText();
 
         if (username.equals("") && password.equals("")) {
+            // Después de la validación del login, cargamos los datos al caché
+            loadEmployeeDataToCache();
             showMainView();
         } else {
             // Manejar error de login
@@ -102,6 +88,35 @@ public class LoginController {
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // Método que carga los datos de los empleados en el caché
+    private void loadEmployeeDataToCache() {
+        if (CacheService.isCacheEmpty()) {
+            try (Connection connection = DatabaseConnection.getConnection()) {
+                String query = "SELECT id, nombres, apellido_paterno, apellido_materno, profesion FROM empleados";
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(query);
+
+                List<Map<String, Object>> employees = new ArrayList<>();
+                while (resultSet.next()) {
+                    Map<String, Object> employeeData = new HashMap<>();
+                    employeeData.put("id", resultSet.getInt("id"));
+                    employeeData.put("nombreCompleto", resultSet.getString("nombres") + " " + resultSet.getString("apellido_paterno") + " " + resultSet.getString("apellido_materno"));
+                    employeeData.put("profesion", resultSet.getString("profesion"));
+                    employees.add(employeeData);
+                }
+
+                // Guardar los datos en el caché
+                CacheService.loadEmployeesIntoCache(employees);
+
+                System.out.println("Datos cargados en el caché");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Datos obtenidos del caché");
         }
     }
 
